8052ec1019c92f77f0ebbcec35c51ec8aed78edb
Vlad <vladbabaev11@gmail.com>
2021-04-27 23:47:18 +0300

    get first step lex enf feature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,23 +29,21 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {}
 
-  getParsedScript(code: string): esprima.Program {
-    return esprima.parseScript(code, {
-      tolerant: true,
-      loc: true,
-    });
-  }
-
   onSubmit() {
     const programmString = this.userInput?.value;
-    console.log('getParsedScript: ', this.getParsedScript(programmString));
-    const programm = this.getParsedScript(programmString) as estree.Program;
-    generateLexEnvRecursive(programm.body);
+    console.log('getParsedScript: ', getParsedScript(programmString));
+    const programm = getParsedScript(programmString) as estree.Program;
+    generateLexEnvRecursive(programm.body, 'global');
+
+    const globaFirstStep = generateFirstStepLexEnv(programm.body);
+    console.log('globaFirstStep: ', globaFirstStep);
   }
 }
 
 //event soursing (event driven approach) каждое распарсивание - это ивент
 //хранить списком FD по uuid, и забить на вложенность
+
+const scopes: any[] = [];
 
 function generateLexEnvRecursive(
   body: Array<
@@ -53,13 +51,77 @@ function generateLexEnvRecursive(
     | estree.Directive
     | estree.Statement
     | estree.ModuleDeclaration
-  >
+  >,
+  name: string
 ) {
   console.log('body: ', body);
+  scopes.push({ name, body });
   body.forEach((entity) => {
     if (entity.type === ProgramBlockEnum.FunctionDeclaration) {
       console.log('entity: ', entity);
-      generateLexEnvRecursive(entity!.body!.body);
+      generateLexEnvRecursive(entity!.body!.body, entity.id!.name);
     }
   });
+
+  console.log('scopes: ', scopes);
+}
+
+function getParsedScript(code: string): esprima.Program {
+  return esprima.parseScript(code, {
+    tolerant: true,
+    loc: true,
+  });
+}
+
+function generateFirstStepLexEnv(
+  body: Array<
+    | estree.FunctionDeclaration
+    | estree.Directive
+    | estree.Statement
+    | estree.ModuleDeclaration
+  >
+) {
+  let firstStepLexEnvBody = [];
+  let firstStepLexEnv = [];
+
+  const functionDeclarations = body.filter(
+    (entity) => entity.type === ProgramBlockEnum.FunctionDeclaration
+  );
+
+  const notFunctionDeclarations = body.filter(
+    (entity) => entity.type !== ProgramBlockEnum.FunctionDeclaration
+  );
+
+  firstStepLexEnvBody = [...functionDeclarations, ...notFunctionDeclarations];
+
+  firstStepLexEnv = firstStepLexEnvBody.map((entity) => {
+    let name = '';
+    let type = '';
+    let kind = '';
+    let value: any = '';
+
+    switch (entity.type) {
+      case ProgramBlockEnum.FunctionDeclaration:
+        name = entity.id!.name;
+        type = entity.type;
+        kind = entity.type;
+        value = 'initialized';
+        return { name, type, value };
+      case ProgramBlockEnum.VariableDeclaration:
+        name = (((entity as estree.VariableDeclaration)
+          .declarations[0] as unknown) as estree.Identifier).name;
+        type = entity.type;
+        kind = entity.kind;
+        if (entity.kind === 'var') {
+          value = 'undefined';
+        } else {
+          value = 'ininitialized';
+        }
+        return { name, type, kind, value };
+      default:
+        return { name: 'lol', type: 'rofl', kind: 'come on', value: 'see ya' };
+    }
+  });
+
+  return firstStepLexEnv;
 }
